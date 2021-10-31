b7b28b2dacf51e2814a8548cfe3796e7bd6892b1
Yair Mazal <yairmazal@gmail.com>
2021-10-31 10:57:52 +0200

    QC file parser

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,6 @@
 from .a_list_format import AList
 from .frame import FramesManager, Frame
 from .custom_exceptions import IncorrectLength, NonBinaryMatrix
-__all__: list[str] = ["AList", "Frame", "FramesManager", "IncorrectLength", "NonBinaryMatrix"]
+from .qc_format import QCFile, InconsistentQCFile
+__all__: list[str] = ["AList", "Frame", "FramesManager", "IncorrectLength", "NonBinaryMatrix", "QCFile",
+                      "InconsistentQCFile"]
